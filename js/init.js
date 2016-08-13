dd9fe617159ac6eec26fe0f044751f6cc9ce154e
Kevin Wilde <kevinwilde2018@u.northwestern.edu>
2016-08-12 21:02:21 -0400

    Navbar

--- js/init.js
+++ js/init.js
@@ -1,8 +1,9 @@
 (function($){
   $(function(){
 
-    // $('.button-collapse').sideNav();
-    Materialize.fadeInImage('.fade');
+    $('.button-collapse').sideNav();
+    // Materialize.fadeInImage('.fade');
+    $('.scrollspy').scrollSpy();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
