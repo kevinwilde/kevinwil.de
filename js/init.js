c5318a681bb7101e7c0d7ac0c1ee080a50beb68d
Kevin Wilde <kevin.wilde@coursehero.com>
2019-01-01 17:19:07 -0600

    fresh for 2019

--- js/init.js
+++
@@ -1,9 +0,0 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-    // Materialize.fadeInImage('.fade');
-    $('.scrollspy').scrollSpy();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
